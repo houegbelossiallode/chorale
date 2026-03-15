5206267a4908b3cad1932bb73be64a101eb02720
houegbelossiallode <houegbelossiallode@gmail.com>
2026-03-15 08:54:52 +0100

    utilisation de \App\Channels\FcmChannel::class et la méthode toFcm() dans RepetitionReminderNotification.php

--- app/Notifications/RepetitionReminderNotification.php
+++ app/Notifications/RepetitionReminderNotification.php
@@ -1,5 +1,4 @@
-﻿<?php
-
+<?php
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
@@ -11,27 +10,16 @@ class RepetitionReminderNotification extends Notification
 {
     use Queueable;
 
-    /**
-     * Create a new notification instance.
-     */
     public function __construct(
         public \App\Models\Repetition $repetition
     ) {
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
     public function via(object $notifiable): array
     {
         return ['mail', 'database', \App\Channels\FcmChannel::class];
     }
 
-    /**
-     * Get the FCM representation of the notification.
-     */
     public function toFcm(object $notifiable): array
     {
         return [
@@ -44,20 +32,12 @@ class RepetitionReminderNotification extends Notification
         ];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
     public function toMail(object $notifiable): \App\Mail\RepetitionReminderMail
     {
         return (new \App\Mail\RepetitionReminderMail($this->repetition))
             ->to($notifiable->email);
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(object $notifiable): array
     {
         return [
